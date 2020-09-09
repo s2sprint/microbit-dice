dcb82cd138e9163fc9abc008d36a8b90fbb2b284
s2sprint <69420623+s2sprint@users.noreply.github.com>
2020-09-09 19:52:22 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,51 @@
+input.onGesture(Gesture.Shake, function () {
+    if (randint(1, 6) == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (randint(2, 6) == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (randint(3, 6) == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (randint(4, 6) == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (randint(5, 6) == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
